57dd8ae3411ea3c07b6623a5c8b84509b6b1f3d7
Jan Kotas <kotasjn@gmail.com>
2023-07-26 17:26:20 +0200

    ✨ Add sample button component

---
+++ feature/presentation/src/main/kotlin/cz/jankotas/screenshottests/feature/presentation/components/Button.kt
@@ -0,0 +1,34 @@
+package cz.jankotas.screenshottests.feature.presentation.components
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun SampleButton(
+    text: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    isEnabled: Boolean = true,
+    textStyle: TextStyle = MaterialTheme.typography.button,
+) {
+    Button(
+        onClick = onClick,
+        modifier = modifier,
+        enabled = isEnabled,
+        shape = RoundedCornerShape((100.dp)),
+    ) {
+        Text(
+            text = text,
+            style = textStyle,
+            color = MaterialTheme.colors.onPrimary,
+            modifier = Modifier.padding(8.dp),
+        )
+    }
+}
