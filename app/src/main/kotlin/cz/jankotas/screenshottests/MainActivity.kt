3db0fa49e6ed4b97e2ad7d8808407269c827061f
Jan Kotas <kotasjn@gmail.com>
2023-07-26 17:26:20 +0200

    ♻️ Cleanup code

--- app/src/main/kotlin/cz/jankotas/screenshottests/MainActivity.kt
+++ app/src/main/kotlin/cz/jankotas/screenshottests/MainActivity.kt
@@ -4,12 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import cz.jankotas.screenshottests.ui.theme.ScreenshotTestsTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,30 +13,12 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ScreenshotTestsTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
+                Scaffold(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background,
                 ) {
-                    Greeting("Android")
+
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier,
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ScreenshotTestsTheme {
-        Greeting("Android")
     }
 }
