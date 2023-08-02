c6507d5ac79113c0461e1b4fffab8ef15d9a933b
Jan Kotas <kotasjn@gmail.com>
2023-08-02 18:46:55 +0200

    🐛 Fix wrong context casting when running paparazzi test

--- coreui/src/main/kotlin/cz/jankotas/screenshottests/coreui/Theme.kt
+++ coreui/src/main/kotlin/cz/jankotas/screenshottests/coreui/Theme.kt
@@ -98,9 +98,12 @@ fun ScreenshotTestsTheme(
     val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
-            val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+            val context = view.context
+            if (context is Activity) {
+                val window = context.window
+                window.statusBarColor = colorScheme.primary.toArgb()
+                WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+            }
         }
     }
 
